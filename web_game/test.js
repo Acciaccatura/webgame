52039943e80435cb10296b9332a5aef17bbb159e
William Chia <william.y.chia@gmail.com>
2016-11-04 12:41:09 -0500

    lmeo

--- web_game/test.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener('keydown', function(event) {
-	console.log(event.keyCode);
-});
-
-document.addEventListener('mousedown', function(event) {
-	console.log(event.button);
-});
